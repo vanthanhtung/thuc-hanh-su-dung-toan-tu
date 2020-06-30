796c6a1ce8c8e7432361beeb3cf1ba1c6241ffc1
vanthanhtung <vanthanhtung1562013@gmail.com>
2020-06-30 10:48:27 +0700

    abc

---
+++ abcs.js
@@ -0,0 +1,8 @@
+let rong;
+let dai;
+rong= prompt("nhập chiều rộng");
+dai= prompt("nhập chiều dài");
+let a = parseInt(rong);
+let b = parseInt(dai);
+let c = a*b;
+document.write("Diện tích hình chữ nhật là: " +c);
